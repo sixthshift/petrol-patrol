6a35c8453ed6b3a3b543cf18007c21f689dd0c13
Jason Huang <sixth.shifts@gmail.com>
2018-08-15 07:40:19 +1000

    Added colour to the statusbar

--- app/styles/styles.js
+++ app/styles/styles.js
@@ -1,13 +1,16 @@
 import { Constants } from 'expo'
 import { StyleSheet } from 'react-native';
 
+import Colours from '../constants/colours';
+
 export default StyleSheet.create({
     container: {
         flex: 1.
     },
     root: {
+        backgroundColor: Colours.primary,
         flex: 1,
-        marginTop: Constants.statusBarHeight
+        paddingTop: Constants.statusBarHeight,
     },
     map: {
         flex: 1,
