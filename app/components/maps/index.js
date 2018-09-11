09a085a7211690de54d45d1ca5dfde23da49451a
Jason Huang <sixth.shifts@gmail.com>
2018-09-12 07:53:29 +1000

    Implemented geofire functionality

--- app/components/maps/index.js
+++ app/components/maps/index.js
@@ -2,6 +2,7 @@ import { Location, MapView, Permissions } from 'expo';
 import React from 'react';
 
 import { defaultRegion } from './constants';
+import firedb from '../../api/firebase';
 import Header from '../header';
 import { noLocationPermissions } from './strings';
 import styles from './styles';
@@ -11,9 +12,44 @@ export default class Map extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            region: defaultRegion
+            region: defaultRegion,
+            stations: [],
         };
+        this.geoQuery = firedb.createGeoQuery(this.state.region);
     }
+
+    componentDidMount() {
+        this._getLocationAsync();
+        this.geoQuery.on('key_entered', (key, location, distance) => {
+            console.log(key + ' ' + location + ' ' + distance);
+        });
+        firedb.updateGeoQuery(this.geoQuery, this.state.region);
+    }
+
+    componentWillUnmount() {
+        this.geoQuery.cancel();
+    }
+
+    _getLocationAsync = async () => {
+        const { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status === 'granted') {
+            const location = await Location.getCurrentPositionAsync({});
+            const region = {
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: this.state.region.latitudeDelta,
+                longitudeDelta: this.state.region.longitudeDelta,
+            };
+            this._onRegionChange(region);
+        } else {
+            ToastAndroid.show(noLocationPermissions, ToastAndroid.LONG);
+        }
+    };
+
+    _onRegionChange = (region) => {
+        this.setState({ ...this.state, region });
+        firedb.updateGeoQuery(this.geoQuery, region);
+    };
 
     static navigationOptions({ navigation }) {
         return {
@@ -29,35 +65,11 @@ export default class Map extends React.Component {
         };
     };
 
-    componentDidMount() {
-        this._getLocationAsync();
-    }
-
-    _getLocationAsync = async () => {
-        const { status } = await Permissions.askAsync(Permissions.LOCATION);
-        if (status === 'granted') {
-            const location = await Location.getCurrentPositionAsync({});
-            const region = {
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude,
-                latitudeDelta: 0.1,
-                longitudeDelta: 0.1,
-            };
-            this.setState({ ...this.state, region });
-        } else {
-            ToastAndroid.show(noLocationPermissions, ToastAndroid.LONG);
-        }
-    };
-
-    changeRegion = (region) => {
-        this.setState({ ...this.state, region });
-    };
-
     render() {
         return (
             <MapView
                 loadingEnabled={true}
-                onRegionChangeComplete={this.changeRegion}
+                onRegionChangeComplete={this._onRegionChange}
                 pitchEnabled={false}
                 region={this.state.region}
                 showsBuildings={false}
