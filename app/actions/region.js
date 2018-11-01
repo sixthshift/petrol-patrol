7f93cdc1007894095237405bed796da8ee8fafb3
Jason Huang <sixth.shifts@gmail.com>
2018-11-01 19:50:34 +1100

    Refactored the region action file into the UI action file

--- app/actions/region.js
+++
@@ -1,4 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const REGION_SET = 'REGION_SET';
-export const setRegionAction = createAction(REGION_SET);
--- app/actions/ui.js
+++ app/actions/ui.js
@@ -8,3 +8,6 @@ export const selectFavouritesAction = createAction(FAVOURITES_SELECT);
 
 export const FUELTYPE_SELECT = 'FUELTYPE_SELECT';
 export const selectFueltypeAction = createAction(FUELTYPE_SELECT);
+
+export const REGION_SET = 'REGION_SET';
+export const setRegionAction = createAction(REGION_SET);
