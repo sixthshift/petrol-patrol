a3d6e58ae9ccf1726d1ae286d4969a23493dcda5
Jason Huang <sixth.shifts@gmail.com>
2019-02-20 07:42:57 +1100

    Added the petrol-patrol-credentials submodule to provide credentials

--- app/api/firebase/index.js
+++ app/api/firebase/index.js
@@ -1,4 +1,4 @@
-import Credentials from './credentials';
+import Credentials from '../../../credentials/petrol-patrol/credentials';
 import FireDB from "./firedb";
 
 export default new FireDB(Credentials);
