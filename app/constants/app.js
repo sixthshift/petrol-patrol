71250df09414fe423777b4272e9ccbf4ec748dfc
Jason Huang <sixth.shifts@gmail.com>
2019-03-01 17:51:11 +1100

    Enabled NRMA analysis

--- app/constants/app.js
+++ app/constants/app.js
@@ -2,13 +2,14 @@
 // The function atob exists in V8 but not JavaScriptCore
 export const __REMOTEDEV__ = (typeof atob !== 'undefined');
 
-// Number of days for the price history chart to fetch
-export const statisticsHistoryRange = 60;
+// Enables the NRMA analysis card in the statistics page
+export const enableAnalysisNRMA = true;
 
 // Number of days for the station price history chart to fetch
 export const priceHistoryRange = 60;
 
+// Number of days for the price history chart to fetch
+export const statisticsHistoryRange = 60;
+
 // The frequency in minutes of database updates
 export const syncFrequency = 30;
-
-export const enableAnalysisNRMA = false;
